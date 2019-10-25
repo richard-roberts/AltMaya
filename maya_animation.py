44574afb4bbdb6b2b6c4deff05033b345ae65e19
Richard Roberts <rykardo.r@gmail.com>
2019-10-25 13:36:22 +1300

    Prototype modules for attributes and animation

---
+++ maya_animation.py
@@ -0,0 +1,73 @@
+import maya
+
+
+class Animation:
+
+    @classmethod
+    def clear_range(cls, attribute_index, start, end):
+        maya.cmds.cutKey(attribute_index.key, time=(start, end))
+
+    @classmethod
+    def add_keyframe(cls, attribute_index, time, value):
+        maya.cmds.setKeyframe(attribute_index.key, time=time, value=value)
+    
+    @classmethod
+    def list_frames_that_have_keyframes(cls, attribute_index):
+        res = maya.cmds.keyframe(attribute_index, query=True)
+        if res is None:
+            return []
+        else:
+            return res
+        
+    @classmethod
+    def is_attribute_animated(cls, attribute_index):
+        return len(cls.list_frames_that_have_keyframes(attribute_index)) != 0
+        
+    @classmethod
+    def list_keyable_attributes(cls, obj):
+        result = maya.cmds.listAttr(obj, shortNames=True, keyable=True)
+        if result is None:
+            return []
+        else:
+            return result
+            
+    @classmethod
+    def list_animated_attributes(cls, obj):
+        return [attr for attr in cls.list_keyable_attributes(obj) if cls.is_attribute_animated(attr)]
+    
+    @classmethod
+    def evaluate_at_frame(cls, attribute_index, frame):
+        return maya.cmds.getAttr(str(attribute_index), t=frame)
+        
+    @classmethod
+    def evaluate_for_timeline(cls, attribute_index):
+        return [
+            cls.evaluate_at_frame(attribute_index, frame)
+            for frame in range(
+                int(Timeline.get_start()),
+                int(Timeline.get_end()) + 1
+            )
+        ]
+    
+    @classmethod
+    def convert_to_free_splines(cls, attribute_index):
+        maya.cmds.keyTangent(attribute_index.key, outTangentType="linear", inTangentType="linear")
+        maya.cmds.keyTangent(attribute_index.key, lock=False)
+        maya.cmds.keyTangent(attribute_index.key, weightedTangents=True)
+        maya.cmds.keyTangent(attribute_index.key, weightLock=False)
+
+    @classmethod
+    def set_keyframe_ingoing_tangent(cls, attribute_index, frame, weight, angle):
+        maya.cmds.keyTangent(
+            attribute_index.key, time=(frame, frame),
+            edit=True, absolute=True,
+            outWeight=weight, outAngle=angle
+        )
+
+    @classmethod
+    def set_keyframe_outgoing_tangent(cls, attribute_index, frame, weight, angle):
+        maya.cmds.keyTangent(
+            attribute_index.key, time=(frame, frame),
+            edit=True, absolute=True,
+            inWeight=weight, inAngle=angle
+        )
---
+++ maya_timeline.py
@@ -0,0 +1,24 @@
+import maya
+
+
+class Timeline:
+    
+    @classmethod
+    def get_start(cls):
+        return maya.cmds.playbackOptions(query=True, minTime=True)
+    
+    @classmethod
+    def get_end(cls):
+        return maya.cmds.playbackOptions(query=True, maxTime=True)
+        
+    @classmethod
+    def list_of_frames(cls):
+        return list(range(int(cls.get_start()), int(cls.get_end()) + 1))
+    
+    @classmethod
+    def set_start(cls, frame):
+        return maya.cmds.playbackOptions(edit=True, minTime=frame)
+    
+    @classmethod
+    def set_end(cls, frame):
+        return maya.cmds.playbackOptions(edit=True, maxTime=frame)
