823c2596ceff0c7a7f7ee747be75c2039471ba51
Richard Roberts <rykardo.r@gmail.com>
2019-10-25 13:49:14 +1300

    Setup __init__ module that imports everything

---
+++ __init__.py
@@ -0,0 +1,8 @@
+
+from .maya_animation import *
+from .maya_attr_index import *
+from .maya_gui import *
+from .maya_history import *
+from .maya_reporting import *
+from .maya_selection import *
+from .maya_timeline import *
