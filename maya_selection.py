c983249eb5848c9686e8b1004a5877d0336f91ea
Richard Roberts <rykardo.r@gmail.com>
2019-12-05 17:08:52 +1300

    Add Selection.third() method

--- maya_selection.py
+++ maya_selection.py
@@ -40,3 +40,14 @@ class Selection:
                 raise ValueError("selection has less than two elements")
             else:
                 return sel[1]
+                
+    @classmethod
+    def third(cls):
+        if cls.is_empty():
+            raise ValueError("selection is empty")
+        else:
+            sel = cls.get()
+            if len(sel) < 3:
+                raise ValueError("selection has less than three elements")
+            else:
+                return sel[2]
