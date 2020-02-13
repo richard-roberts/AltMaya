b3e46c97d523319facf85247b8c09e554bdc8fc6
Richard Roberts <rykardo.r@gmail.com>
2020-02-13 17:42:02 +1300

    Reload function

--- __init__.py
+++ __init__.py
@@ -10,11 +10,24 @@ import maya_reporting
 import maya_selection 
 import maya_timeline 
 
+reload(maya_animation)
+reload(maya_attr_index)
+reload(maya_functions)
+reload(maya_gui)
+reload(maya_history)
+reload(maya_mesh)
+reload(maya_object_index)
+reload(maya_plugins)
+reload(maya_reporting)
+reload(maya_selection)
+reload(maya_timeline)
+
 Animation = maya_animation.Animation
 AnimationCurve = maya_animation.AnimationCurve
 AttributeIndex = maya_attr_index.AttributeIndex
 Functions = maya_functions.Functions
 StandardMayaWindow = maya_gui.StandardMayaWindow
+Ask = maya_gui.Ask
 History = maya_history.History
 Vertex = maya_mesh.Vertex
 Triangle = maya_mesh.Triangle
@@ -24,35 +37,4 @@ Plugins = maya_plugins.Plugins
 Report = maya_reporting.Report
 Selection = maya_selection.Selection
 Timeline = maya_timeline.Timeline
-
-# def _reload():
-#     reload(maya_animation)
-#     reload(maya_attr_index)
-#     reload(maya_functions)
-#     reload(maya_gui)
-#     reload(maya_history)
-#     reload(maya_mesh)
-#     reload(maya_object_index)
-#     reload(maya_plugins)
-#     reload(maya_reporting)
-#     reload(maya_selection)
-#     reload(maya_timeline)
-    
-#     # StandardMayaWindow
-#     global Animation, AnimationCurve, AttributeIndex, Functions, History, Vertex, Triangle, Mesh, ObjectIndex, Plugins, Report, Selection, Timeline
-    
-#     Animation = maya_animation.Animation
-#     AnimationCurve = maya_animation.AnimationCurve
-#     AttributeIndex = maya_attr_index.AttributeIndex
-#     Functions = maya_functions.Functions
-#     # StandardMayaWindow = maya_gui.StandardMayaWindow
-#     History = maya_history.History
-#     Vertex = maya_mesh.Vertex
-#     Triangle = maya_mesh.Triangle
-#     Mesh = maya_mesh.Mesh
-#     ObjectIndex = maya_object_index.ObjectIndex
-#     Plugins = maya_plugins.Plugins
-#     Report = maya_reporting.Report
-#     Selection = maya_selection.Selection
-#     Timeline = maya_timeline.Timeline
 
